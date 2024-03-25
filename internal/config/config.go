b5f6481df9cc9e213aa47ed7d5754864e2d78a9b
i.abdukhoshimov <i.abdukhoshimov@gmail.com>
2024-03-25 13:32:48 +0500

    static env added

--- internal/config/config.go
+++ internal/config/config.go
@@ -94,8 +94,8 @@ type Config struct {
 	EmailNotificationConfig EmailNotificationConfig `yaml:"notification_service"`
 
 	Casbin struct {
-		ConfigPath string `env:" ../transport/grpc/middleware/rbac.conf"`
-		PolicyPath string `env:" ../transport/grpc/middleware/policy_effect.csv"`
+		ConfigPath string `env:"CASBIN_CONFIG_PATH"`
+		PolicyPath string `env:"CASBIN_POLICY_PATH"`
 	}
 }
 
